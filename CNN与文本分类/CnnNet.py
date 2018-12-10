2753c0409c3ab824eef36b8f2be8081d32b66896
TaoEnd <769524295@qq.com>
2018-12-10 10:24:58 +0800

    cnn for text 2

--- CNN与文本分类/CnnNet.py
+++ CNN与文本分类/CnnNet.py
@@ -24,3 +24,31 @@ class CnnNet(object):
             self.embedded_chars = tf.nn.embedding_lookup(self.w, self.x)
             # 扩展维度，-1表示在最后一维上再增加一维
             self.embedded_chars_expanded = tf.expand_dims(self.embedded_chars, -1)
+
+        # 定义卷积层和池化层
+        # 邮件在处理成一个矩阵时，每一个邮件对应一个矩阵，矩阵的宽是邮件中每个词的
+        # 词向量长度，矩阵的长是邮件中词的个数，为了使得所有邮件矩阵的大小相同，
+        # 因此规定所有矩阵的长为单词数量最多的邮件对应的单词数量
+        pooled_outputs = []
+        for i, conv_size in enumerate(conv_sizes):
+            with tf.name_scope("conv-maxpool-%d" % conv_size):
+                conv_shape = [conv_size, embedding_size, 1, num_convs]
+                # truncated_normal()：利用3sigma原则，从截断的正态分布中输出随机值，
+                # 当随机值大于或者小于2倍偏差值时，就丢弃该随机值，重新选择一个
+                # stddev代表标准差
+                w = tf.Variable(tf.truncated_normal(conv_shape, stddev=0.3), name="w")
+                b = tf.Variable(tf.random_normal(shape=[num_convs]), name="b")
+                conv = tf.nn.conv2d(self.embedded_chars_expanded, w, strides=[1, 1, 1, 1],
+                                    padding="VALID", name="conv")
+                # 将向量b加到矩阵conv的每一行，b与conv中每一行的长度相等
+                relu = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
+                # ksize中第二个参数表示height，第三个参数表示width
+                pooled = tf.nn.max_pool(relu, ksize=[1, sequence_length-conv_size+1, 1, 1],
+                                        strides=[1, 1, 1, 1], padding="VALID", name="pool")
+                pooled_outputs.append(pooled)
+
+        num_convs_tool = num_convs * len(conv_sizes)
+        # 在pooled_outputs的第四维上进行连接
+        self.pool = tf.concat(3, pooled_outputs)
+        # 把原本重叠在一起的卷积层全部竖起来放，并且连接在一起，以便后面进行全连接操作
+        self.pool_flat = tf.reshape(self.pool, [-1, num_convs])
